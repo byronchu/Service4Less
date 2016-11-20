a9a804c7ac1e11af107af766f3a7b6693e101716
Byron <chu.byron@yahoo.com>
2016-11-20 11:44:34 +0000

    modified services controller and _form current_user.services.build

--- controllers/services_controller.rb
+++ controllers/services_controller.rb
@@ -24,7 +24,7 @@ class ServicesController < ApplicationController
   # POST /services
   # POST /services.json
   def create
-    @service = Service.new(service_params)
+    @service = current_user.services.build(service_params)
 
     respond_to do |format|
       if @service.save
